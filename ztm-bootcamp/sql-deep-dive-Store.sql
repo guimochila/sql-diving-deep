af7cdfb38a9d1d1841a39fcac1cc5c261e3a8d26
Guilherme <gui.mochila@gmail.com>
2024-05-07 17:29:08 +0200

    feat(ztm-bootcamp): Operator precedence

--- ztm-bootcamp/sql-deep-dive-Store.sql
+++ ztm-bootcamp/sql-deep-dive-Store.sql
@@ -20,3 +20,16 @@ and income <= 50000;
 -- What is the averageincome between the ages of 20 and 50?
 select avg(income) from customers
 where age > 20 and age < 50;
+
+-- Select people either under 30 or over 50 with an income above 50000
+-- that are from either Japan or Australia
+select * from customers 
+where income > 50000
+and (age < 30 or age >= 50)
+and (country = 'Japan' or country = 'Australia');
+
+-- What was our total sales in June of 2004 for orders over 100 dollars
+select sum(totalamount) from orders
+where (orders.orderdate >= '2004-06-01' and orderdate <= '2004-06-30')
+and totalamount  > 100;
+
